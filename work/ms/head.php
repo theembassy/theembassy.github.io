0344f98450d75f65feacd7c3b5146d761153c9d3
The Embassy <31825483+theembassy@users.noreply.github.com>
2017-11-29 09:40:10 +0100

    yes

---
+++ work/ms/head.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en-US"><head>
+<meta charset="utf-8" />
+<meta name="description" content="Newbuild" />
+<meta name="keywords" content="euromix, unity, beats" />
+<meta name="viewport" content="width=device-width" />
+
+<link rel="shortcut icon" href="https://theembassy.github.io/images/pearr.ico" />    
+<link href="css/style.css" rel="stylesheet" />
+
+<title>MARCUS S&Ouml;DERLUND</title>
+
+</head>
+
+<body>
+
+<div class="wrapper">
+	<div class="container">
+
+        <!--/ HEAD -->
+
+        <ul class="contact">
+            <li><script src="js/blink.js"></script></li>
+            <li><a href="tel:+46739380209">+46739380209</a></li>
+            <li>info@marcussoderlund.se</li>
+        </ul>
+
+        <ul class="represent">
+            <li>Representation:</li>
+            <li>SWEDEN <a href="http://folkefilm.se/category/directors/marcus-soderlund/" target="_blank" class="textlink">Folke Film</a></li>
+            <li>UK <a href="http://www.academyfilms.com/marcus-sderlund" target="_blank" class="textlink">Academy Films</a></li>
+            <li>US <a href="http://www.resetcontent.com/marcus-soderlund" target="_blank" class="textlink">Reset Content</a></li>
+        </ul>
---
+++ work/ms/index.php
@@ -0,0 +1,68 @@
+<?php include('head.php'); ?>
+
+
+<!--/ BILDER OCH BILDTEXTER BIG -->
+        
+		<div class="image-zoom-container">
+            
+			<div class="zoom-container">
+				<a href="persil_footprints.php">
+					<span class="zoom-caption">
+						<h3>Persil-Footprints</h3>
+					</span>
+                    <img src="images/1.jpg" />
+				</a>
+			</div>
+			<div class="zoom-container">
+				<a href="kuoni_find_your_amazing.php">
+					<span class="zoom-caption">
+						<h3>Kuoni - Find Your Amazing</h3>
+					</span>
+					<img src="images/2.jpg" />
+				</a>
+			</div>
+			<div class="zoom-container">
+				<a href="volvo_the_swell.php">
+					<span class="zoom-caption">
+						<h3>Volvo - The Swell</h3>
+					</span>
+					<img src="images/3.jpg" />
+				</a>
+			</div>
+			<div class="zoom-container">
+				<a href="vodafone_the_call.php">
+					<span class="zoom-caption">
+						<h3>Vodafone - The Call</h3>
+					</span>
+					<img src="images/4.jpg" />
+				</a>
+			</div>
+			<div class="zoom-container">
+				<a href="avis_unlock_the_world.php">
+					<span class="zoom-caption">
+						<h3>Avis - Unlock The World</h3>
+					</span>
+					<img src="images/5.jpg" />
+				</a>
+			</div>
+			<div class="zoom-container">
+				<a href="mcd_drive.php">
+					<span class="zoom-caption">
+						<h3>McDonald's - Drive</h3>
+					</span>
+					<img src="images/6.jpg" />
+				</a>
+			</div>
+
+		</div>
+
+
+<!--/ MENU --> 
+
+        <ul class="grid-nav">
+            <li><a href="move.php" class="textlink">MOVEMENT</a></li>
+        </ul>
+
+
+
+<?php include('foot.php'); ?>
