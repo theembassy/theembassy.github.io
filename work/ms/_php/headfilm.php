4b5cb03a1b1d791b31ddba8ecf9aabe77d8544d3
theembassy <embarrassing@gmail.com>
2018-02-06 18:37:53 +0100

    up

--- work/ms/_php/headfilm.php
+++ work/ms/_php/headfilm.php
@@ -1,31 +1,36 @@
 <!doctype html>
-<html lang="en-US"><head>
-<meta charset="utf-8" />
-<meta name="keywords" content="..." />
-<meta name="description" content="..." />
-<meta name="viewport" content="width=device-width" />
+<html lang="en-US">
 
-<link rel="shortcut icon" href="https://theembassy.github.io/images/pearr.ico" />
-<link href="css/style.css" rel="stylesheet" />
+<head>
+	<meta charset="utf-8" />
+	<meta name="description" content="Newbuild" />
+	<meta name="keywords" content="euromix, unity, beats" />
+	<meta name="viewport" content="width=device-width" />
 
-<title>MARCUS S&Ouml;DERLUND</title>
+	<link rel="shortcut icon" href="https://theembassy.github.io/images/pearr.ico" />
+	<link href="css/style.css" rel="stylesheet" />
+
+	<title>MARCUS S&Ouml;DERLUND</title>
 
 </head>
 
 <body>
 
-<div class="wrapper">
-	<div class="container">
+	<div class="wrapper">
+		<div class="container">
 
-        <ul class="contact">
-            <li><a href="http://marcussoderlund.se">MARCUS S&Ouml;DERLUND</a></li>
-            <li><a href="tel:+46739380209">+46739380209</a></li>
-            <li>info@marcussoderlund.se</li>
-        </ul>
+			<!--/ HEAD -->
 
-         <ul class="represent">
-            <li>Representation:</li>
-            <li>SWEDEN <a href="http://folkefilm.se/marcus-soderlund" target="_blank" class="textlink">Folke Film</a></li>
-            <li>UK <a href="http://www.academyfilms.com/marcus-sderlund" target="_blank" class="textlink">Academy Films</a></li>
-            <li>US <a href="http://www.resetcontent.com/marcus-soderlund" target="_blank" class="textlink">Reset Content</a></li>
-        </ul>
+			<ul class="contact">
+				<!--/<li><script src="js/blink.js"></script></li>-->
+				<h1><a href="http://marcussoderlund.se">MARCUS S&Ouml;DERLUND</a></h1>
+				<li><a href="tel:+46739380209">+46739380209</a></li>
+				<li>info@marcussoderlund.se</li>
+			</ul>
+
+			<ul class="represent">
+				<li>Representation:</li>
+				<li>SWEDEN <a href="http://folkefilm.se/category/directors/marcus-soderlund/" target="_blank" class="textlink">Folke Film</a></li>
+				<li>UK <a href="http://www.academyfilms.com/marcus-sderlund" target="_blank" class="textlink">Academy Films</a></li>
+				<li>US <a href="http://www.resetcontent.com/marcus-soderlund" target="_blank" class="textlink">Reset Content</a></li>
+			</ul>
