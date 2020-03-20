cd12357d67011060cdaeba5ca04c856dbf8fcccb
The Embassy <embarrassing@gmail.com>
2020-03-20 21:30:02 +0100

    up

---
+++ ms1/_php/index.php
@@ -0,0 +1,65 @@
+<?php include('head.php'); ?>
+
+
+<!--/ BILDER OCH BILDTEXTER BIG -->
+
+<div class="image-zoom-container">
+
+	<div class="zoom-container">
+		<a href="yung_lean_rbs.php">
+		<span class="zoom-caption">
+			<h3>Yung Lean<br>Red Bottom Sky</h3>
+		</span>
+			<img src="images/1.jpg" />
+	</a>
+	</div>
+	<div class="zoom-container">
+		<a href="nature_valley_force_of_nature.php">
+		<span class="zoom-caption">
+			<h3>Nature Valley<br>Force of Nature</h3>
+		</span>
+		<img src="images/2.jpg" />
+	</a>
+	</div>
+	<div class="zoom-container">
+		<a href="dhl_mongolia.php">
+		<span class="zoom-caption">
+			<h3>DHL<br>Mongolia</h3>
+		</span>
+		<img src="images/3.jpg" />
+	</a>
+	</div>
+	<div class="zoom-container">
+		<a href="red_cross_no_one_should_be_left_alone.php">
+		<span class="zoom-caption">
+			<h3>Red Cross<br>No One Should Be Left Alone</h3>
+		</span>
+		<img src="images/4.jpg" />
+	</a>
+	</div>
+	<div class="zoom-container">
+		<a href="honda_real_dreamers.php">
+		<span class="zoom-caption">
+			<h3>Honda<br>Real Dreamers</h3>
+		</span>
+		<img src="images/5.jpg" />
+	</a>
+	</div>
+	<div class="zoom-container">
+		<a href="birds_eye_with_love.php">
+		<span class="zoom-caption">
+			<h3>Birds Eye<br>With Love</h3>
+		</span>
+		<img src="images/6.jpg" />
+	</a>
+	</div>
+
+</div>
+
+<!--/ MENU -->
+
+<ul class="grid-nav">
+	<li><h2><a href="move.php">MOVEMENT</a></h2></li>
+</ul>
+
+<?php include('foot.php'); ?>
---
+++ ms1/_php/persil_footprints.php
@@ -0,0 +1,16 @@
+<?php include('headfilm.php'); ?>
+
+<!--/ VIDEOLINK --> 
+		<div class="responsive-container">
+            <iframe src="https://player.vimeo.com/video/132921269" width=”800” height=”450” frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe> 
+        </div>
+		
+<!--/ FILMINFO --> 
+        <ul class="filmnfo">
+			<li>CLIENT: Persil</li>
+			<li>AGENCY: DLKW Lowe</li>
+			<li>TITLE: Footprints</li>
+            <li>PRODUCER: <a href="http://www.academyfilms.com/projects/marcus-sderlund" target="_blank" class="textlink">Academy Films</a></li>
+        </ul>
+
+<?php include('footfilm.php'); ?>
